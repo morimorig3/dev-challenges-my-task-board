519bbd1d1fb0f24ac4f1515983dac4c394a92cc3
Naoto Morishita <mori.nto@outlook.jp>
2025-11-19 15:05:20 +0900

    feat: front Task Card

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="font-[Outfit] p-10">
+      <main>
+        <h1 className="text-[40px]">My Task Board</h1>
+        <ul>
+          <li>
+            <div className="flex p-5 gap-5 rounded-2xl items-start bg-[#A0ECB1]">
+              <p className="text-2xl w-12 h-12 grid place-items-center rounded-xl bg-white">
+                📚
+              </p>
+              <div className="grow">
+                <p className="text-xl font-semibold">Task Todo</p>
+                <div>
+                  description,descriptiondescriptiondescription
+                  <br />
+                  descriptiondescription
+                </div>
+              </div>
+              <button>
+                <p className="text-2xl w-12 h-12 grid place-items-center rounded-xl bg-[#32D657]">
+                  ☑️
+                </p>
+              </button>
+            </div>
+          </li>
+        </ul>
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
